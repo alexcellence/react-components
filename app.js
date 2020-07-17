b3cb7b4970dfac992953dacfac50ffcfb213fb65
Alex Wu <alexcellence@gmail.com>
2020-07-17 01:45:42 -0700

    Create a grocery list

--- app.js
+++ app.js
@@ -1,17 +1,78 @@
 // TODO
-var GroceryList = () => (
+
+// props = listOfGroceries={['Bread', 'Milk']}
+
+// props.listOfGroceries = ['Bread', 'Milk']
+
+var groceryArray = ["Bread", "Milk", "Fish"];
+
+var GroceryList = (props) => (
   <ul>
-    <Milk />
-    <Bread />
+    {props.listOfGroceries.map(item => <GroceryListItem itemKey={item}/>)}
   </ul>
-);
+)
 
-ReactDOM.render(<GroceryList />, document.getElementById('app'));
+// props.listOfGroceries.map(item => <GroceryListItem itemKey={item}/>)
 
-var Milk = () => (
-  <li>Milk</li>
-);
+// var GroceryList = (props) => (
+//   <ul>
+//     {props.listOfGroceries.map(item => <GroceryListItem itemKey={item}/>)}
+//   </ul>
+// )
 
-var Bread = () => (
-  <li>Bread</li>
-);
+// const numbers = [1, 2, 3, 4, 5];
+// const listItems = numbers.map((number) =>
+//   <li>{number}</li>
+// );
+// console.log(listItems);
+
+// {props.listOfGroceries.map(item => <GroceryListItem itemKey={item}/>)} = {[<li>{props.itemKey}</li>, <li>{props.itemKey}</li>, <li>{props.itemKey}]}
+
+{/* var GroceryList = (props) => (
+  <ul>
+    <li>{props.itemKey}</li>
+    <li>{props.itemKey}</li>
+    <li>{props.itemKey}</li>
+  </ul>
+) */}
+
+
+// var GroceryListItem = (props) => (
+//   <li>{props.itemKey}</li>
+// )
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bold: false
+    };
+  }
+
+  onListMouseOver() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListMouseOver.bind(this)}>{this.props.itemKey}</li>
+    )
+  }
+
+}
+
+// onMouseOver
+
+
+var App = () => (
+  <GroceryList listOfGroceries={['Bread', 'Milk', 'Fish']}/>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
